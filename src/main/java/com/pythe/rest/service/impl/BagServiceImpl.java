eb1531371a952bec8d0cfb37a3dec48db8a51d1a
zhangyb3 <cheung29@foxmail.com>
2017-12-20 17:23:45 +0800

    201712201723

--- src/main/java/com/pythe/rest/service/impl/BagServiceImpl.java
+++ src/main/java/com/pythe/rest/service/impl/BagServiceImpl.java
@@ -1,7 +1,9 @@
 package com.pythe.rest.service.impl;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,19 +12,34 @@ import org.springframework.stereotype.Service;
 import com.alibaba.fastjson.JSONObject;
 import com.pythe.common.pojo.PytheResult;
 import com.pythe.common.utils.FactoryUtils;
+import com.pythe.common.utils.HttpClientUtil;
+import com.pythe.common.utils.JsonUtils;
 import com.pythe.mapper.TblAccountMapper;
 import com.pythe.mapper.TblBagRecordMapper;
 import com.pythe.mapper.TblBillMapper;
+import com.pythe.mapper.TblCustomerMapper;
+import com.pythe.mapper.TblDealerMapper;
 import com.pythe.mapper.TblStoreMapper;
 import com.pythe.pojo.TblAccount;
 import com.pythe.pojo.TblBagRecord;
 import com.pythe.pojo.TblBill;
+import com.pythe.pojo.TblCustomer;
 import com.pythe.pojo.TblStore;
 import com.pythe.pojo.TblStoreExample;
 import com.pythe.rest.service.BagService;
 
 @Service
 public class BagServiceImpl implements BagService {
+	
+	@Value("${WX_APPID}")
+	private String WX_APPID;
+
+	@Value("${WX_APPSECRET}")
+	private String WX_APPSECRET;
+	
+	@Value("${GIFT_NOTIFY_TEMPLATE_ID}")
+	private String GIFT_NOTIFY_TEMPLATE_ID;
+	
 	@Autowired
 	private TblStoreMapper storeMapper;
 
@@ -32,9 +49,14 @@ public class BagServiceImpl implements BagService {
 	@Autowired
 	private TblAccountMapper accountMapper;
 	
+	@Autowired
+	private TblCustomerMapper customerMapper;
 	
 	@Autowired
 	private TblBillMapper billMapper;
+	
+	@Autowired
+	private TblDealerMapper dealerMapper;
 	
 	
 	// BILL
@@ -99,6 +121,48 @@ public class BagServiceImpl implements BagService {
 		account.setInAmount(account.getInAmount() +GIVE_BAG_MONEY);
 		account.setGivingAmount(account.getGivingAmount() +GIVE_BAG_MONEY);
 		accountMapper.updateByPrimaryKey(account);
+		
+		
+		//向消费用户发送服务通知
+		String url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential" + "&appid=" + WX_APPID
+				+"&secret=" + WX_APPSECRET;
+		String result = HttpClientUtil.doGet(url, null);
+		System.out.println(result);
+		String access_token = JSONObject.parseObject(result).getString("access_token");
+		System.out.println("=====================>"+access_token);
+		
+		JSONObject notifyParameters = new JSONObject();
+		
+		Map<String, Object> datas = new HashMap<String, Object>();
+
+		Map<String, String> keyValue1 = new HashMap<String, String>();
+		keyValue1.put("value", "￥38.00");
+		datas.put("keyword1", keyValue1);
+		
+		Map<String, String> keyValue2 = new HashMap<String, String>();
+		keyValue2.put("value", dealerMapper.selectByPrimaryKey(dealerId).getName());
+		datas.put("keyword2", keyValue2);
+		
+		Map<String, String> keyValue3 = new HashMap<String, String>();
+		keyValue3.put("value", "价值￥68的垫套，￥20预存费用");
+		datas.put("keyword3", keyValue3);
+		
+		Map<String, String> keyValue4 = new HashMap<String, String>();
+		keyValue4.put("value", bill.getTime().toString());
+		datas.put("keyword4", keyValue4);
+		
+		notifyParameters.put("touser", customerMapper.selectByPrimaryKey(customerId).getOpenId());
+		notifyParameters.put("template_id",GIFT_NOTIFY_TEMPLATE_ID);
+		notifyParameters.put("form_id", bill.getPrepayId());
+		notifyParameters.put("page", "pages/index/index");
+		notifyParameters.put("data", datas);
+		notifyParameters.put("emphasis_keyword", "keyword1.DATA");
+		
+		String xw_url = "https://api.weixin.qq.com/cgi-bin/message/wxopen/template/send?access_token="+access_token;
+		String str = HttpClientUtil.doPostJson(xw_url, JsonUtils.objectToJson(notifyParameters));
+		
+		System.out.println("===================> notify template result: " + str);
+		
 		return PytheResult.ok("记录成功");
 	}
 
